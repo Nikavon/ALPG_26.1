aa42ac5f8bc6f3eaedbe27b1edce382daefe919c
Anna <48450318+Nikavon@users.noreply.github.com>
2026-06-05 17:41:12 -0300

    Create programa.py

---
+++ Lista6/q3/programa.py
@@ -0,0 +1,5 @@
+from conversores import celsius_para_fahrenheit, fahrenheit_para_celsius
+
+if __name__ == "__main__":
+    print("30 graus Celsius em Fahrenheit é:", celsius_para_fahrenheit(30))
+    print("86 graus Fahrenheit em Celsius é:", fahrenheit_para_celsius(86))
